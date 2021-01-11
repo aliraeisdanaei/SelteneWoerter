b4e43203055108279281855b2247be81af29a2c4
Ali Raeisdanaei <mrrookie2@gmail.com>
2021-01-11 15:32:36 -0500

    Developped the script to find the rare words.

--- rare_finder.py
+++ rare_finder.py
@@ -5,21 +5,29 @@ from nltk import FreqDist #to get frequencu table
 import os #to get fiel directory 
 import glob #to get file directories at the same time 
 import re #regular expressions
-import csv #csv files 
 import pandas as pd # to make data frames 
 
 import sys #for the argv use
 
-top_n_rare = 5
+# if no value is given as a parameter the top 20 rare words are found
+top_n_rare = 20 
 if len(sys.argv) != 0 and type(sys.argv[0]) == int:
 	top_n_rare = sys.argv[0]
 
+#it has been assumed that the freqTable is uptodate
+# perhaps a script here is needed to  automatically determine if it requires updating
+freqTable = pd.read_csv('./frequencyTable.txt') 
+freqTable.columns = ['word', 'frequency'] # gives column headers
+# print(freqTable)
+
 
 cwd = os.getcwd() #getting current directory 
-files = glob.glob(cwd + '/*html') #getting all file names 
+files = glob.glob(cwd + '/*transkript.html') #getting all file names with this specified pattern
 
-#result = []
-FreqDistacc = FreqDist() #getting an empty FreqDst object
+def determinePodcastNumber(file) -> str:
+	# this function assumes this format for the transkript names
+	# egp<number>_transkript.html
+	return (str(file)).split('egp')[1].split('_')[0]
 
 for file in files: 
 	opener = codecs.open(file, "r", "utf-8") #opening all files
@@ -36,16 +44,18 @@ for file in files:
 	words = word_tokenize(cleaned) #tokenezing cleaned corpus
 	lowerCase = [each_string.lower() for each_string in words]
 
+	currentWords = pd.DataFrame({'word':lowerCase})
+	# print(currentWords)
+	
+	# takes the natural joic of currentWords & freqTable & sorts them based on frequency
+	# occuring_words = lowerCase_set & freqTable_words_Desc
+	# top_rare_list = list(occuring_words)[:top_n_rare]
+	occuring_words = pd.merge(currentWords, freqTable).sort_values(by=['frequency'])
+	print(occuring_words)
 
-	FreqDistacc.update(lowerCase) #adding them to the freqDist object. 
-	#result.append(fd)
+	top_rare = occuring_words.head(top_n_rare)
+	print("Top " + str(top_n_rare) + " seltene Wörter in egp" + determinePodcastNumber(file) + ":", list(top_rare.iloc[:, 0].values))
 
-sortedCounts = sorted(FreqDistacc.items() , key = lambda x: x[1] ,
-                       reverse = True) #sortning them
+	rare_filename = 'egp' + determinePodcastNumber(file) + "_selteneWörter.txt" #setting a file name based on the podcast number
+	top_rare.to_csv(rare_filename, header=False, index=False, mode='a') #saving it 
 
-dictionary = dict(sortedCounts) #turning it into a dictionary
-print(len(dictionary))
-df = pd.DataFrame.from_dict(dictionary, orient='index') #turning it into a data frame
-#print(df)
-
-df.to_csv('freqTable.txt', header=False, index=True, mode='a') #saving it 
